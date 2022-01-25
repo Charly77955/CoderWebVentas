6eb768e8ac6bf94dc87490b16b7fe048d4477412
unknown <desantiscarlos87@gmail.com>
2022-01-24 21:54:28 -0300

    CodeRR

--- src/components/ProductList/ItemListContainer.js
+++ src/components/ProductList/ItemListContainer.js
@@ -1,9 +1,5 @@
 import React, { useEffect, useState } from "react";
-<<<<<<< HEAD
 import ProductStyle from './ProductList';
-=======
-import ProductStyle from './ProductStyle';
->>>>>>> 0218a63 (Para chequear-CarlosDeSantis)
 
 
 
@@ -25,13 +21,8 @@ export default function Productos() {
                     },
                     {
                         id: '2',
-<<<<<<< HEAD
                         titulo: 'Set Matero 2',
                         descripcion: 'Set Matero con mate, bombilla, termo y bolso para llevar',
-=======
-                        titulo: 'Set de te',
-                        descripcion: 'Set para te, jarron,  y bolso para llevar',
->>>>>>> 0218a63 (Para chequear-CarlosDeSantis)
                         precio: '$3000',
                         imagen: 'https://image.freepik.com/foto-gratis/tetera-tradicional-china-o-japonesa-taza-te-mantel-individual_23-2148186327.jpg'
                     },
