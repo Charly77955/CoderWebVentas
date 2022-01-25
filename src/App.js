bf6d66f61b2c068f044a4b9238a4cdf0dbd69912
unknown <desantiscarlos87@gmail.com>
2022-01-24 23:32:38 -0300

    Clase7DetailCarlosDeSantis

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar/Navbar';
 import Productos from './components/ProductList/ItemListContainer';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Carrito from './components/ShoppingCart';
+import ItemDetail from './components/ItemDetail/ItemDetail';
 
 function App() {
   return (
@@ -16,10 +17,11 @@ function App() {
      
      
      <Route path="Carrito" element={<Carrito/>}/>
-      
+      <Route path="Detallebtn" element={<ItemDetail/>}/>
+
      <Route/>
      <Route path="productos" element={<Productos/>}/>
-      
+      <Route path="ItemDetail" element={<ItemDetail/>}/>
      <Route/>
      </Routes>
      
--- src/components/ProductList/ItemListContainer.js
+++ src/components/ProductList/ItemListContainer.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import ProductStyle from './ProductList';
+import ProductStyle from './ProductStyle';
 
 
 
