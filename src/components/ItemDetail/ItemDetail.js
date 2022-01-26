55db5da983bc3323836cdfb669cb553c14866863
unknown <desantiscarlos87@gmail.com>
2022-01-25 22:04:23 -0300

    DetalleCarlosDeSantis

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -5,7 +5,7 @@ import { ItemDetailStyle } from "./ItemDetailStyle";
 
 export default function ItemDetail () {
 
-    const  [ PromesaDetallesbtn, setPromesaDetallesbtn ] = useState( [false])
+    const  [ PromesaDetallesbtn, setPromesaDetallesbtn ] = useState(false)
     const  [ DetalleProducto, setDetalleProducto ] = useState(null)
 
     const  Detallesbtn = new Promise ((resolve, reject) => {
@@ -13,8 +13,8 @@ export default function ItemDetail () {
             resolve(
                 {
                     id: '1',
-                    titulo: 'Set Matero',
-                    descripcion: 'Set de te, jarron, tazas y bolso para llevar',
+                    titulo: 'Set matero',
+                    descripcion: 'Set matero con mate, termo, bombilla y bolso para llevar incluido',
                     precio: '$2000',
                     imagen: 'https://image.freepik.com/foto-gratis/te-infusible-sobre-madera_144627-20539.jpg'
                 }
@@ -31,9 +31,11 @@ export default function ItemDetail () {
     return(
         <div>
             {(PromesaDetallesbtn) ?
-            <div><ItemDetailStyle key={Detallesbtn.id} item={Detallesbtn}></ItemDetailStyle></div>
+            <ItemDetailStyle key={DetalleProducto?.id} item={DetalleProducto}></ItemDetailStyle>
                 :
-                <>Loading...</>
+                <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
             }
         </div>
     )
--- src/components/ItemDetail/ItemDetailStyle.js
+++ src/components/ItemDetail/ItemDetailStyle.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-export const ItemDetailStyle = () => {
-    
+export const ItemDetailStyle = (props) => {
+    const { item } = props;
     return (
-        <div className="card mb-3" style='45em'>
+        <div className="card mb-3">
             <div className="row g-0">
                 <div className="col-md-4">
-                    <img src="https://image.freepik.com/foto-gratis/te-infusible-sobre-madera_144627-20539.jpg" class="img-fluid rounded-start" alt="..."/>
+                <img src="https://image.freepik.com/foto-gratis/te-infusible-sobre-madera_144627-20539.jpg" className="img-fluid rounded-start" alt="..."/>
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        <h5 className="card-title">{item?.titulo}</h5>
+                        <p className="card-text">Set matero con mate, termo, bombilla y bolso para llevar incluido</p>
+                        <p className="card-text"><small className="text-muted">Consulta para incluir otros productos del catalogo</small></p>
                     </div>
                 </div>
             </div>
